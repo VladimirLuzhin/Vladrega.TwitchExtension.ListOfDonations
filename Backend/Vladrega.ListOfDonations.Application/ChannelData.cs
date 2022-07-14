4ee268ab2a78bf0c398efc8394e775b91eed804d
Vladimir Luzhin <reddemonvovas@yandex.ru>
2022-07-15 00:32:35 +1000

    add picture for readme and comments

--- Backend/Vladrega.ListOfDonations.Application/ChannelData.cs
+++ Backend/Vladrega.ListOfDonations.Application/ChannelData.cs
@@ -1,11 +1,23 @@
 ﻿namespace Vladrega.ListOfDonations.Application;
 
+/// <summary>
+/// Данные канала
+/// </summary>
 public record ChannelData
 {
+    /// <summary>
+    /// Донаты
+    /// </summary>
     public IEnumerable<Donations> Donations { get; init; }
     
+    /// <summary>
+    /// Тема для донатеров
+    /// </summary>
     public Theme Theme { get; init; }
 
+    /// <summary>
+    /// .ctor
+    /// </summary>
     public ChannelData(IEnumerable<Donations> donations, Theme theme)
     {
         Donations = donations;
--- Backend/Vladrega.ListOfDonations.Application/Theme.cs
+++ Backend/Vladrega.ListOfDonations.Application/Theme.cs
@@ -1,8 +1,22 @@
 ﻿namespace Vladrega.ListOfDonations.Application;
 
+/// <summary>
+/// Темы для TOP 3 донатеров
+/// </summary>
 public enum Theme : int
 {
+    /// <summary>
+    /// Дефолтная тема (нет оформления)
+    /// </summary>
     Default = 0,
+    
+    /// <summary>
+    /// Heroes of Might and Magic 3
+    /// </summary>
     Heroes3 = 1,
+    
+    /// <summary>
+    /// Disciples 2
+    /// </summary>
     Disciples2 = 2
 }
